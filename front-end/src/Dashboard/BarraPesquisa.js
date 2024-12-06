db63b849666ea4832291b1f068364743c72fa77f
migueltsw <miguel.almeida@escolar.ifrn.edu.br>
2024-12-05 22:26:34 -0300

    Filtro barra de pesquisa

--- front-end/src/Dashboard/BarraPesquisa.js
+++ front-end/src/Dashboard/BarraPesquisa.js
@@ -1,11 +1,22 @@
-export default function BarraPesquisa(){
-    return(
-        <div class="container input-group mt-5 w-25 justify-content-center align-items-center">
-            <input type="text" class="form-control" placeholder="Pesquisar" />
-            <div class="input-group-append w-25">
-                <button class="btn btn-dark" type="button">
-                    <i class="fa fa-search"></i>
-                </button>
+
+export default function BarraPesquisa({ searchQuery, setSearchQuery }) {
+    return (
+        <div className="container mt-5 d-flex justify-content-center">
+            <div className="input-group w-50 w-md-50 w-lg-25">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Pesquisar"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <div className="input-group-append">
+                    <button className="btn btn-dark" type="button">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                        </svg>
+                    </button>
+                </div>
             </div>
         </div>
     );
