6b6c3356d7ca3e45392968dcf803849b82b577f1
Edunogueiraa <eduardonogueira105@gmail.com>
2024-12-10 23:16:33 -0300

    Correção de rota

--- back-end/routes/web.php
+++ back-end/routes/web.php
@@ -2,10 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/{any}', function () {
-//     return view('welcome');
-// })->where('any', '.*');
-
-Route::get('/', function () {
+Route::get('/{any}', function () {
     return view('welcome');
-});
+})->where('any', '.*');
+
